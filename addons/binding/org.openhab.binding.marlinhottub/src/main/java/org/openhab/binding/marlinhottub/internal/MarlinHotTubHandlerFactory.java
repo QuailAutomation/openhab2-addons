e08c91b0838835986cb16fab8f2b0252067e97d9
HentschelT <thomas@hentschel.net>
2017-12-06 01:32:20 -0800

    cleaning local repo mess

--- addons/binding/org.openhab.binding.marlinhottub/src/main/java/org/openhab/binding/marlinhottub/internal/MarlinHotTubHandlerFactory.java
+++ addons/binding/org.openhab.binding.marlinhottub/src/main/java/org/openhab/binding/marlinhottub/internal/MarlinHotTubHandlerFactory.java
@@ -1,43 +1,40 @@
 /**
- * Copyright (c) 2014,2017 by the respective copyright holders.
+ * Copyright (c) 2010-2017 by the respective copyright holders.
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.binding.marlinhottub.internal;
-
-import static org.openhab.binding.marlinhottub.MarlinHottubBindingConstants.*;
 
 import java.util.Collections;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.binding.marlinhottub.handler.MarlinHottubHandler;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.binding.marlinhottub.MarlinHotTubBindingConstants;
+import org.openhab.binding.marlinhottub.handler.MarlinHotTubHandler;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * The {@link MarlinHottubHandlerFactory} is responsible for creating things and thing
+ * The {@link MarlinHotTubHandlerFactory} is responsible for creating things and thing
  * handlers.
  *
  * @author Thomas Hentschel - Initial contribution
  */
 @Component(service = ThingHandlerFactory.class, immediate = true, configurationPid = "binding.marlinhottub")
 @NonNullByDefault
-public class MarlinHottubHandlerFactory extends BaseThingHandlerFactory {
+public class MarlinHotTubHandlerFactory extends BaseThingHandlerFactory {
 
-    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
+    @SuppressWarnings("null")
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections
+            .singleton(MarlinHotTubBindingConstants.THING_TYPE_MARLINHOTTUB);
 
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
@@ -48,8 +45,8 @@ public class MarlinHottubHandlerFactory extends BaseThingHandlerFactory {
     protected @Nullable ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (thingTypeUID.equals(THING_TYPE_SAMPLE)) {
-            return new MarlinHottubHandler(thing);
+        if (thingTypeUID.equals(MarlinHotTubBindingConstants.THING_TYPE_MARLINHOTTUB)) {
+            return new MarlinHotTubHandler(thing);
         }
 
         return null;
